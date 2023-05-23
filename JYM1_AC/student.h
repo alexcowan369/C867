040f89ad386bd448b6ebee0c10518fa0fe6cffcb
alexcowan369 <132611480+alexcowan369@users.noreply.github.com>
2023-05-23 15:04:28 -0700

    Update student.h

--- JYM1_AC/student.h
+++ JYM1_AC/student.h
@@ -4,13 +4,16 @@
 #include <iostream>
 using namespace std;
 
-//Don't forget class definitions here
+// class of student defined
 class Student
 {
+    // public for usage across the program
 public:
+    // constant integer value of 3 assigned to tableValue
    const static int tableValue = 3;
-
+    // following are private and can only be accessed from the methods below
 private:
+    // methods below which hold data for student objects
    string studentID;
    string firstName;
    string lastName;
@@ -20,10 +23,12 @@ private:
    DegreeProgram dgram;
   
 public:
-   //default constructor
+   // default constructor
    Student();
   
-   //full constructor
+   // full constructor, matching the name of the class
+   // parameters below for what the constructor will take 
+   // allows us to pass variables into the object when instantiated
    Student(
        string studentID,
        string firstName,
@@ -34,7 +39,7 @@ public:
        DegreeProgram dgram
    );
 
-   //getter for the instances
+   // getter for the instances, these return the value of corresponding method below
    string getStudentID();
    string getFirstName();
    string getLastName();
@@ -43,7 +48,7 @@ public:
    int* getDaysToComplete();
    DegreeProgram getDegreeProgram();
 
-   //setters
+   // setters, these take a parameter and set the value to the parameter inputted
    void setID(string studentID);
    void setFirst(string firstName);
    void setLast(string lastName);
@@ -53,7 +58,7 @@ public:
    void setDegreeProgram(DegreeProgram dgram);
    void print();
 
-   //destructor
+   // destructor to clean up and resources used to free space
    ~Student();
 
 };
